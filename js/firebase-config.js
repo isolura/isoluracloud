7bd5d12bc562d15fd864ce11afa0594b940c9718
isolura <isoslithers@gmail.com>
2026-05-23 16:28:06 -0400

    Update firebase-config.js
    added firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,11 +5,20 @@
 // Project Settings → Your apps → SDK setup and configuration
 // ============================================================
 
-export const firebaseConfig = {
-  apiKey:            "PASTE-YOUR-API-KEY-HERE",
-  authDomain:        "PASTE-YOUR-AUTH-DOMAIN-HERE",
-  projectId:         "PASTE-YOUR-PROJECT-ID-HERE",
-  storageBucket:     "PASTE-YOUR-STORAGE-BUCKET-HERE",
-  messagingSenderId: "PASTE-YOUR-MESSAGING-SENDER-ID-HERE",
-  appId:             "PASTE-YOUR-APP-ID-HERE"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6QjOtaf3M-wYWzdUmCz-t_mu2h4zPm2c",
+  authDomain: "isoluracloud.firebaseapp.com",
+  projectId: "isoluracloud",
+  storageBucket: "isoluracloud.firebasestorage.app",
+  messagingSenderId: "869617091580",
+  appId: "1:869617091580:web:a922fc562b446792ee6879"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
